84b47675095de4a2704c1366d5437acc2284ebf5
Ronak-IIITD <ronak24482@iiitd.ac.in>
2026-01-30 17:25:19 +0530

    some fixes and updates

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ronak-anand/Desktop/studysync-ai/backend/venv
+set -gx VIRTUAL_ENV /home/ronak-anand/Desktop/acadira-ai/backend/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
